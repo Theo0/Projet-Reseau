a4986593db6ec623d8580959fb3e9a0a20b3cab7
Theo0 <theo341@gmail.com>
2014-12-02 19:42:58 +0100

    Mise en fonction client

--- ClientServeurNonConcurrent/clientTCP.cpp
+++ ClientServeurNonConcurrent/clientTCP.cpp
@@ -12,97 +12,10 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]){
-
-if(argc != 2)
-{cout << "Nombre de paramètres incorrect, il manque l'adresse du serveur" << endl;
-exit(1);}
-
-//Creation de la boite de récéption 
-Sock brCli(SOCK_STREAM, 0);
-int descBrCli;
-
-//Test de la boite de récéption et récupération du descripteur
-if(brCli.good())
-  {descBrCli = brCli.getsDesc();}
-else
-  {cout << "Erreur boite reception client" << endl; exit(1);}
-
-//Création de la BR publique
-SockDist brPub(argv[1], short(10514));
-struct sockaddr_in * adrBrPub = brPub.getAdrDist();
-
-int lgAdrBrPub = sizeof(struct sockaddr_in);
-
-//Connexion au serveur
-int connexion = connect(descBrCli, (struct sockaddr *)adrBrPub, lgAdrBrPub);
-if(connexion == -1) {cout << "Echec de la connexion" << endl; exit(1);}
-
-
-char tamponEnvoi[100];
-char tamponOrdre[20];
-char tamponChemin[200];
-char tamponEnvoiFichier[100000];
-char tamponReception[100000];
-
-
-
-	while(1){
-		cout << "Entrez 0 pour recevoir un fichier, 1 pour en déposer un, exit pour quitter : " ;
-		cin >> tamponOrdre;
-
-		//Vérification de la demande d'arrêt ("stop" entré au clavier par l'utlisateur)
-		if(!strcmp(tamponOrdre, "exit"))
-		    {exit(0);}
-
-		//On envoi l'ordre au serveur
-		int envoi = send(descBrCli, tamponOrdre, strlen(tamponOrdre), 0);
-		if(envoi == -1){ cout << "Echec de l'envoi de la commande" << endl; exit(1);}
-
-		if(atoi(tamponOrdre) == 0){
-			cout << "On rente dans recep" << endl;
-			//RECEPTION DE FICHIER PAR LE CLIENT *************************************************************
-			cout << "Entrez le nom du fichier souhaité (exit pour quitter) : ";
-			cin >> tamponEnvoi;
-			envoi = send(descBrCli, tamponEnvoi, strlen(tamponEnvoi), 0);
-			if(envoi == -1){ cout << "Echec de l'envoi" << endl; exit(1);}
-			else {cout << "Demande envoyée" << endl;}
-
-
-			int reception = recv(descBrCli, tamponReception, sizeof(tamponReception), 0);
-			if(reception == -1){ cout << "Echec de la reception" << endl; exit(1);}
-
-			//On vérifie la réponse du serveur
-			string part(tamponReception);
-			string reponse(part.substr(0, reception));
-			if(reponse == "Le fichier n'existe pas")
-			{
-				cout << "le fichier demandé n'existe pas" << endl;
-			}
-			else{
-
-				cout << "Entrer le nom sous lequel vous souhaitez sauvegarder le fichier : " ;
-				char nomfichier[100];
-				cin >> nomfichier;
-
-				//Création du fichier reçu
-				FILE *fichier = NULL;
-				fichier =  fopen (nomfichier, "w+");
-				if(fichier == NULL)
-				       { printf ("Erreur a l'ouverture du fichier\n"); }
-
-
-				//Remplissage du fichier reçu
-				fwrite(tamponReception,1,reception, fichier);
-				cout << "Fichier reçu" << endl;
-				fclose(fichier);
-				cout << "\n" << endl;
-			}
-			//FIN RECEPTION DE FICHIER PAR LE CLIENT *********************************************************
-		}
-		else if(atoi(tamponOrdre) == 1){
-			//ENVOI DE FICHIER DU CLIENT VERS LE SERVEUR ******************************************************
-
+void envoyer(int descBrCli){
+	//ENVOI DE FICHIER DU CLIENT VERS LE SERVEUR ******************************************************
+			char tamponChemin[200];
+			int envoi;
 			//L'utilisateur entre le chemin du fichier qu'il souahite envoyer
 			cout << "Entrez le chemin du fichier que vous souhaitez envoyer : ";
 			cin >> tamponChemin;
@@ -147,6 +60,105 @@ char tamponReception[100000];
       		fclose(fichierenvoi);    
       		}
 			//FIN ENVOI DE FICHIER DU CLIENT VERS LE SERVEUR **************************************************
+}
+
+void reception(int descBrCli){
+	char tamponReception[100000];
+	char tamponEnvoi[100];
+	int envoi;
+	cout << "On rente dans recep" << endl;
+			//RECEPTION DE FICHIER PAR LE CLIENT *************************************************************
+			cout << "Entrez le nom du fichier souhaité (exit pour quitter) : ";
+			cin >> tamponEnvoi;
+			envoi = send(descBrCli, tamponEnvoi, strlen(tamponEnvoi), 0);
+			if(envoi == -1){ cout << "Echec de l'envoi" << endl; exit(1);}
+			else {cout << "Demande envoyée" << endl;}
+
+
+			int reception = recv(descBrCli, tamponReception, sizeof(tamponReception), 0);
+			if(reception == -1){ cout << "Echec de la reception" << endl; exit(1);}
+
+			//On vérifie la réponse du serveur
+			string part(tamponReception);
+			string reponse(part.substr(0, reception));
+			if(reponse == "Le fichier n'existe pas")
+			{
+				cout << "le fichier demandé n'existe pas" << endl;
+			}
+			else{
+
+				cout << "Entrer le nom sous lequel vous souhaitez sauvegarder le fichier : " ;
+				char nomfichier[100];
+				cin >> nomfichier;
+
+				//Création du fichier reçu
+				FILE *fichier = NULL;
+				fichier =  fopen (nomfichier, "w+");
+				if(fichier == NULL)
+				       { printf ("Erreur a l'ouverture du fichier\n"); }
+
+
+				//Remplissage du fichier reçu
+				fwrite(tamponReception,1,reception, fichier);
+				cout << "Fichier reçu" << endl;
+				fclose(fichier);
+				cout << "\n" << endl;
+			}
+			//FIN RECEPTION DE FICHIER PAR LE CLIENT *********************************************************
+}
+
+int main(int argc, char *argv[]){
+
+if(argc != 2)
+{cout << "Nombre de paramètres incorrect, il manque l'adresse du serveur" << endl;
+exit(1);}
+
+//Creation de la boite de récéption 
+Sock brCli(SOCK_STREAM, 0);
+int descBrCli;
+
+//Test de la boite de récéption et récupération du descripteur
+if(brCli.good())
+  {descBrCli = brCli.getsDesc();}
+else
+  {cout << "Erreur boite reception client" << endl; exit(1);}
+
+//Création de la BR publique
+SockDist brPub(argv[1], short(10514));
+struct sockaddr_in * adrBrPub = brPub.getAdrDist();
+
+int lgAdrBrPub = sizeof(struct sockaddr_in);
+
+//Connexion au serveur
+int connexion = connect(descBrCli, (struct sockaddr *)adrBrPub, lgAdrBrPub);
+if(connexion == -1) {cout << "Echec de la connexion" << endl; exit(1);}
+
+
+
+char tamponOrdre[20];
+
+char tamponEnvoiFichier[100000];
+
+
+
+
+	while(1){
+		cout << "Entrez 0 pour recevoir un fichier, 1 pour en déposer un, exit pour quitter : " ;
+		cin >> tamponOrdre;
+
+		//Vérification de la demande d'arrêt ("stop" entré au clavier par l'utlisateur)
+		if(!strcmp(tamponOrdre, "exit"))
+		    {exit(0);}
+
+		//On envoi l'ordre au serveur
+		int envoi = send(descBrCli, tamponOrdre, strlen(tamponOrdre), 0);
+		if(envoi == -1){ cout << "Echec de l'envoi de la commande" << endl; exit(1);}
+
+		if(atoi(tamponOrdre) == 0){
+			reception(descBrCli);
+		}
+		else if(atoi(tamponOrdre) == 1){
+			envoyer(descBrCli);
 			
 		}
 			else{

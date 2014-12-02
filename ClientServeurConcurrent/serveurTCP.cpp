b0f061c964b4532c6cf1238e443a48d74a808a6f
tchambon <tchambon@v32.etu.info-ufr.univ-montp2.fr>
2014-12-02 17:45:11 +0100

    ajout serveur concurrent v1

---
+++ ClientServeurConcurrent/serveurTCP.cpp
@@ -0,0 +1,183 @@
+#include <cstring> 
+#include <string>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "sockdist.h"
+#include "sock.h"
+#include <pwd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <mutex>
+
+using namespace std;
+
+void *connection_handler(void *);
+pthread_t thread_id;
+std::mutex monlock;
+
+int *envoi(void *descBrCv){
+  cout << "on entre en reception" << endl;
+  monlock.lock();
+  char tamponReception[100];
+  char tamponEnvoi[100000];
+
+  //Reception du message : nom du fichier
+    int reception = recv(descBrCv, tamponReception, sizeof(tamponReception), 0);
+    if(reception == -1) cout << "Echec de la reception" << endl;
+
+    //Recupération du message envoyé qui est transformé en string pour avoir le nom du fichier souhaité
+    string part(tamponReception);
+    string file(part.substr(0, reception));
+    cout << "Nom du fichier :" << file << endl;
+    monlock.unlock();
+    //On essaie d'ouvrir le fichier souhaité par le client
+    FILE *fichier = NULL;
+    fichier = fopen(file.c_str(),  "r");
+        
+    
+    //Si fopen a échoué, le fichier n'existe pas, on envoi une réponse négative au client
+    if (fichier == NULL){
+      char tamponEnvoi[]="Le fichier n'existe pas";
+      cout << "Le fichier demandé par le client n'existe pas" <<  endl;
+      int envoi = send(descBrCv, tamponEnvoi, strlen(tamponEnvoi), 0);
+      if(envoi == -1) cout << "Echec de l'envoi de la réponse négative" << endl;
+      else
+      {cout << "Réponse envoyée" << endl;}
+     }
+
+    //Sinon on envoi le fichier
+    else{
+    //On transforme le fichier afin de pouvoir l'envoyer via send
+      fseek(fichier, 0, SEEK_END);
+      long fsize = ftell(fichier);
+      fseek(fichier, 0, SEEK_SET);
+      char *string = malloc(fsize + 1);
+      fread(string, fsize, 1, fichier);
+      string[fsize] = 0;
+
+      //Envoi du fichier (string)
+      int envoi = send(descBrCv, string, strlen(string), 0);
+      if(envoi == -1) cout << "Echec de l'envoi du fichier" << endl;
+      else
+          {cout << "Fichier envoyé correctement" << endl; }
+
+      //On ferme le fichier
+      fclose(fichier);
+  }
+}
+
+
+int reception(int descBrCv){
+  monlock.lock();
+  char tamponNomFichier[200];
+  char tamponReceptionFichier[100000];
+      int reception = recv(descBrCv, tamponNomFichier, sizeof(tamponNomFichier), 0);
+
+      if(!strcmp(tamponNomFichier, "error")){
+        if(reception == -1){ cout << "Echec de la reception nom du fichier" << endl;}
+        else{
+          int reception = recv(descBrCv, tamponReceptionFichier, sizeof(tamponReceptionFichier), 0);
+          monlock.unlock();
+          if(reception == -1){ cout << "Echec de la reception du fichier" << endl;}
+          else{
+            //Création du fichier de destination
+            FILE *fichierRecu = NULL;
+            fichierRecu =  fopen (tamponNomFichier, "w+");
+            if(fichierRecu == NULL)
+               { printf ("Erreur a l'ouverture du fichier\n"); }
+
+            fwrite(tamponReceptionFichier,1,reception, fichierRecu);
+            cout << "Fichier reçu" << endl;
+            fclose(fichierRecu);
+            cout << "\n" << endl;
+          }
+        }
+      }
+}
+
+
+int main(int argc, char *argv[]){
+//Préparation BR publique
+Sock brPub(SOCK_STREAM, (short)(10514),0);
+int descBrPub;
+
+//Verification du bind
+if(brPub.good()){
+  descBrPub = brPub.getsDesc();
+}
+
+int ecoute = listen(descBrPub, 5);
+//Verification de l'écoute
+if(ecoute == -1) cout << "Echec de listen" << endl;
+
+
+struct sockaddr_in brCv;
+socklen_t lgbrCv = sizeof(struct sockaddr_in);
+
+int descBrCv;
+//Le serveur accepte un nouveau client (appel système testé)
+while( (descBrCv = accept(descBrPub, (struct sockaddr *)&brCv, (socklen_t*)&lgbrCv)) )
+{
+    puts("Connection accepted");
+         
+        if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &descBrCv) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+}
+
+if(descBrCv == -1) {
+  cout << "Echec de l'accept" << endl;}
+
+} 
+
+void *connection_handler(void *descBrCv){
+    //création du tampons
+
+  char tamponOrdre[1];
+  int sock = *(int*)descBrCv;
+  int ordre;
+
+
+   while( (ordre = recv(sock , tamponOrdre , sizeof(tamponOrdre) , 0)) > 0 )  
+  {
+    cout << atoi(tamponOrdre) << endl;
+    if(atoi(tamponOrdre) == 0){
+        
+           if( pthread_create( &thread_id , NULL ,  envoi , sock) < 0)
+            {
+                perror("could not create thread");
+                return 1;
+            }
+    }
+    else if(atoi(tamponOrdre) == 1){
+           if( pthread_create( &thread_id , NULL ,  reception , sock) < 0)
+            {
+                perror("could not create thread");
+                return 1;
+            }
+    }
+    else{
+          cout << "Instruction inconnue reçue : " << tamponOrdre << endl;
+        }
+    memset(tamponOrdre, 0, 10);    
+ }
+
+    if(ordre == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(ordre == -1)
+    {
+        perror("recv failed");
+    }
+         
+    return 0;
+  
+  }

95e1943ed00f8a7a2391616c593e2815fc0f8d00
Théo <theo341@gmail.com>
2015-01-18 21:16:56 +0100

    Update lisp.lisp

--- lisp.lisp
+++ lisp.lisp
@@ -1,105 +1,143 @@
 ;TESTS :
-(LISPTOLI '(defun test_set (x) (setf x 1)) ())
-=>(:SET-VAR (:VAR 0) (:CONST 1))
+(let x)
+(setf x 4)
 
-(LISPTOLI '(if (< x 0) 1 0) '(x))
-=> (:IF (:CALL < ((:VAR 0) (:CONST 0))) (:CONST 1) (:CONST 0))
+(LISPTOLI '(defun test_plus (x) (+ x 3)) '(x))
+=> (:CALL + ((:VAR 0) (:CONST 3)))
 
-(LISPTOLI '(defun factorielle (n) (if (<= n 0) 1 (* n (factorielle (- n 1))))) ())
-=> (:IF (:CALL <= ((:VAR 0) (:CONST 0))) (:CONST 1) (:CALL * ((:VAR 0) (:MCALL FACTORIELLE ((:CALL - ((:VAR 0) (:CONST 1))))))))
+(EVALLI '(:CALL + ((:VAR 0) (:CONST 3))) (make-array 1 :INITIAL-ELEMENT x))
+=> 7
 
-(LISPTOLI '(defun fibonacci (n) (if (<= n 2) 1 (+ (fibonacci (- n 1)) (fibonacci (- n 2))))) ())
-=> (:IF (:CALL <= ((:VAR 0) (:CONST 2))) (:CONST 1) (:CALL + ((:MCALL FIBONACCI ((:CALL - ((:VAR 0) (:CONST 1))))) (:MCALL FIBONACCI ((:CALL - ((:VAR 0) (:CONST 2))))))))
+(LISPTOLI '(defun test_if (x) (if (< 2 3) 1 0)) '())
+=> (:IF (:CALL < ((:CONST 2) (:CONST 3))) (:CONST 1) (:CONST 0))
 
-(LISPTOLI '(defun test_quote (x) '(setf x 1)) ())
-=> (:CONST (SETF X 1))
+(EVALLI '(:IF (:CALL > ((:CONST 2) (:CONST 3))) (:CONST 1) (:CONST 0)) '())
+=> 0
 
-(LISPTOLI '(defun test_progn_sur_factorielle (x) (progn (defun factorielle (n) (if (<= n 0) 1 (* n (factorielle (- n 1)))))) ()) ())
-=> (:PROGN ((:IF (:CALL <= ((:VAR 0) (:CONST 0))) (:CONST 1) (:CALL * ((:VAR 0) (:UNKNOWN (FACTORIELLE (- N 1)) (N)))))))
+(EVALLI '(:IF (:CALL < ((:CONST 2) (:CONST 3))) (:CONST 1) (:CONST 0)) '())
+=> 1
 
-(LISPTOLI '(defun test_loop () (loop for i from 1 to 10 do (print i))) ())
-=>(:CALL LOOP ((:UNKNOWN FOR) (:UNKNOWN I) (:UNKNOWN FROM) (:CONST 1) (:UNKNOWN TO) (:CONST 10) (:UNKNOWN DO) (:CALL PRINT ((:UNKNOWN I)))))
+(EVALLI '(:CALL > ((:VAR 0) (:CONST 3))) (make-array 1 :INITIAL-ELEMENT x))
+=> T
+
+(EVALLI '(:CALL < ((:VAR 0) (:CONST 3))) (make-array 1 :INITIAL-ELEMENT x))
+=> NIL
+
+(LISPTOLI '(defun test_if_avec_variable (x) (if (< x 3) 1 0)) '(x))
+=> (:IF (:CALL < ((:VAR 0) (:CONST 3))) (:CONST 1) (:CONST 0))
+
+(EVALLI '(:IF (:CALL < ((:VAR 0) (:CONST 3))) (:CONST 1) (:CONST 0)) (make-array 1 :INITIAL-ELEMENT x))
+=> 0
+
+(EVALLI '(:IF (:CALL > ((:VAR 0) (:CONST 3))) (:CONST 1) (:CONST 0)) (make-array 1 :INITIAL-ELEMENT x))
+=> 1
+
+(defun test_plus (x) (+ x 3))
+(MEVAL '(test_plus 7))
+=> 10
+
+(defun fibonacci (n) (if (<= n 2) 1 (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
+(MEVAL '(fibonacci 6))
+=> 8
+
+(defun soustractionRecursive (x) (if (= x 0) x (- 1 (soustractionRecursive (- x 1)))))
+(MEVAL '(soustractionRecursive 3))
+=> 1
+
+(defun factorielle (n) (if (<= n 0) 1 (* n (factorielle (- n 1)))))
+(MEVAL '(factorielle 6))
+=> 720
+
+(defun test_if (x) (if (< x 3) 1 0))
+(MEVAL '(test_if 9))
+=> 0
+(MEVAL '(test_if 1))
+=> 1
+
+(MEVAL '(MEVAL '(fibonacci 6)))
+=> 8
+
+(MEVAL '(MEVAL '(MEVAL '(fibonacci 6))))
+=> 8
+
+(defun test_loop () (loop for i from 1 to 10 do (print i)))
+(MEVAL '(test_loop ))
+=> 1 2 3 4 5 6 7 8 9 10 NIL
 
 
-(defun LISPTOLI (expr env) 
-	;Si c'est un atome
-	(if (atom expr) 
-		;Si c'est une constante 
-		(if (constantp expr) 
-			;On créé une liste avec :const et la valeur de la constante 
-			(list :const expr) 
-			;Sinon (c'est une variable d'environnement) alors on regarde si on connait l'emplacement de la variable 
-			(let (pos (position expr env))  ;; POSITION RETOURNE LA POSITION DE expr dans ENV (ou NIL si inconnue), et on affecte cela à la variable locale pos
-				;Si la position est connue 
-				(if pos 
-					;On créé une liste avec :var et la position de cette variable 
-					(list :var pos) 
-					;Sinon c'est une variable inconnue, on créé donc une liste avec :unknown et la variable inconnue 
-					(list :unknown expr)
-				)
+
+(defun EVALLI (expr env) 
+	(cond 
+		;Si c'est une constante
+		((eq (car expr ) :const ) ;On affiche le cdr (la valeur de la constante)
+			(car (cdr expr ) ) 
+		)
+		 ;Si c'est une variable
+		((eq (car expr ) :var ) ;On la récupère (sa valeur) dans le tableau des variables d'environnement
+		 	(aref env (car (cdr expr) ) ) 
+		 )
+		;Si c'est un setf
+		((eq (car expr ) :set-var )
+		;On récupère la variable dans le tableau des variables d'environnement
+		 	(aref env (second (cadr expr ) ) ) 
+		 ;Et on affecte à cette variable l'évaluation du cddr de l'expression en passant l'environnement courant 
+		  	(setf (EVALLI (cddr expr ) env ) ) 
+		)
+		;Si c'est une expression conditionnelle
+		((eq (car expr ) :if ) 
+			(if
+			;On évalue le test
+				(EVALLI (second expr ) env )
+				;On évalue l'expression de retour dans le cas positif du test
+				(EVALLI (third expr ) env ) 
+				;On évalue l'expression de retour dans le cas négatif du test
+				(EVALLI (fourth expr ) env ) 
 			)
 		)
-
-		;Si ce n'est pas un atome on récupère le car de l'expr et le cdr de l'expr 
-		(let 
-			((fun (car expr)) (args (cdr expr))) ;; on stocke la premiere partie de expr dans fun, et le reste dans args
-			(cond 
-				;Si c'est une fonction 
-				((eq 'defun fun) 
-					;On transcrit le corps de la fonction en LI en lui passant les paramètres et on associe :defun au nom de la fonction 
-					(setf (get (first args) :defun ) ;; on stocke le mot clef :defun à la place du premier argument
-						(LISPTOLI (third args) (second args)) ;; et on repasse lisptoli sur la suite des arguements
-					)
-				)
-				;Si c'est une initialisation de variable ou de fonction 
-				((eq 'setf fun)
-					;Si le second élément de l'expression est un symbole 
-					(if (symbolp (second expr)) 
-						;Alors on créé une liste avec :set-var et on transcrit en LI la seconde partie de l'expression ainsi que la troisième partie de celle-ci 
-						(list :set-var (LISPTOLI (second expr) env) 
-							(LISPTOLI (third expr ) env )
-						)
-						;Sinon (c'est une valeur) on créé donc simplement une liste avec :setf et on transcrit en LI la seconde partie de l'expression 
-						(list :setf (LISPTOLI (second expr ) env ) )
-					)
-				)
-
-				;Si c'est une expression commençant par une quote 
-				((eq 'quote fun ) 
-					;On fait sauter la quote, on passe donc a une constante contenant l'expression en LISP car on ne sait pas ce qu'il se cache derrière "(first args)"
-					(list :const (first args ) ) 
-				)
-				;Si c'est une expression conditionnelle 
-				((eq 'if fun) 
-					;On concatène :if et on fait un MAPLISPTOLI des arguments avec l'environnement pour faire le LI de l'expression et des cas 
-					(cons :if (MAPLISPTOLI args env ) ) 
-				)
-				;Si c'est une expression appellant progn 
-				((eq 'progn fun ) 
-				;On créé une liste avec :progn et MAPLISPTOLI des arguments avec l'environnement
-					(list :progn (MAPLISPTOLI args env ) ) )
-				;Si c'est un appel à une fonction déclarée par l'utilisateur 
-				((not (null (get fun :defun )))
-				 ;On créé une liste avec :mcall, le nom de fonction et la suite qui sera transcrit en LI dans MAPLISPTOLI
-					(list :mcall fun (MAPLISPTOLI args env ) ) )
-				;Si c'est une fonction inconnue 
-				((not (fboundp fun ) ) ;On créé une liste avec :unknown et la concaténation du nom de fonction et des arguments en LISP ainsi que l'environnement
-					(list :unknown (cons fun args ) env ) )
-				;Si c'est une fonction prédéfinie 
-				((fboundp fun ) ;On créé une liste avec :call, le nom de la fonction et la suite qui sera transcrit en LI dans MAPLISPTOLI
-					(list :call fun (MAPLISPTOLI args env ) ) )
+		;Si c'est un appel de fonction primitive
+		(
+			(eq (car expr ) :call )
+			 ;On applique cette fonction primitive à la suite de l'expression
+			(apply (cadr expr ) (MAPEVALLI (first (cddr expr ) ) env ) ) 
+		)
+		;Si c'est une fonction inconnue on teste l'évaluation de la transcription en LI du reste de l'expression
+		((eq (car expr ) :unknown ) 
+			(EVALLI (LISPTOLI (second expr ) (third expr ) ) env ) 
+		)
+		;Si c'est un appel de fonction créée par l'utilisateur
+		((eq (car expr ) :mcall ) ;On affecte à une variable du nom de la fonction le retour de l'évaluation de l'expression
+			(let* 
+				((fun (GETLIDEFUN (second expr ) ) ) (nenv (make-array (+ 1 (car fun ) ) ) ) )
+				(EVALLI (car (cddr fun ) ) (MAKEENV (car (cddr expr ) ) env nenv 1 ) )
 			)
 		)
-	)
-)      
-
-(defun MAPLISPTOLI (lexpr env) 
-	;Si c'est un atome 
-	(if (atom lexpr ) ;On ne fait rien
-		NIL 
-		;Sinon on transcrit en LI le premier élément et on réalise une récursion sur le reste 
-		(cons 
-			(LISPTOLI (first lexpr ) env ) (MAPLISPTOLI (rest lexpr ) env ) 
+		;Si c'est un progn d'une expression alors on évalue la suite de l'expression en ne retournant que la valeur du dernier appel récursif
+		((eq (car expr ) :progn)
+			(car (last (MAPEVALLI (car (cdr expr ) ) env ) ) )
 		)
+    )
+)
+
+(defun MAPEVALLI (lexpr env)
+	(if (null lexpr ) ;; si lexpr est nul
+		NIL ;; on retourne NIL
+		(cons (EVALLI (first lexpr ) env ) (MAPEVALLI (rest lexpr ) env ) ) ;; sinon on applique la fonction sur le reste des arguments
 	)
 )
+
+
+(defun MAKEENV (args env nenv index)
+	(if (null args )
+		nenv
+		(progn (setf (aref nenv index ) (EVALLI (car args ) env ) ) (MAKEENV (cdr args ) env nenv (+ 1 index ) ) )
+	)
+)
+
+(defun MEVAL (expr)
+	(EVALLI (LISPTOLI expr ()) () )
+)
+
+(defun GETLIDEFUN (fun) 
+	(get fun :defun )
+)
+

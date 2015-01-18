1b29af8154938ac47a9881682c66625bdce8b131
Théo <theo341@gmail.com>
2015-01-18 21:18:43 +0100

    Update lisp.lisp

--- lisp.lisp
+++ lisp.lisp
@@ -1,143 +1,88 @@
-;TESTS :
-(let x)
-(setf x 4)
+;CREATION de la Machine Virtuelle avec initialisation des piles et des pointeurs
+;Ici, la pile de Controle et la pile de Donnees sont separees. Le role de chacune est ainsi plus explicites.
 
-(LISPTOLI '(defun test_plus (x) (+ x 3)) '(x))
-=> (:CALL + ((:VAR 0) (:CONST 3)))
-
-(EVALLI '(:CALL + ((:VAR 0) (:CONST 3))) (make-array 1 :INITIAL-ELEMENT x))
-=> 7
-
-(LISPTOLI '(defun test_if (x) (if (< 2 3) 1 0)) '())
-=> (:IF (:CALL < ((:CONST 2) (:CONST 3))) (:CONST 1) (:CONST 0))
-
-(EVALLI '(:IF (:CALL > ((:CONST 2) (:CONST 3))) (:CONST 1) (:CONST 0)) '())
-=> 0
-
-(EVALLI '(:IF (:CALL < ((:CONST 2) (:CONST 3))) (:CONST 1) (:CONST 0)) '())
-=> 1
-
-(EVALLI '(:CALL > ((:VAR 0) (:CONST 3))) (make-array 1 :INITIAL-ELEMENT x))
-=> T
-
-(EVALLI '(:CALL < ((:VAR 0) (:CONST 3))) (make-array 1 :INITIAL-ELEMENT x))
-=> NIL
-
-(LISPTOLI '(defun test_if_avec_variable (x) (if (< x 3) 1 0)) '(x))
-=> (:IF (:CALL < ((:VAR 0) (:CONST 3))) (:CONST 1) (:CONST 0))
-
-(EVALLI '(:IF (:CALL < ((:VAR 0) (:CONST 3))) (:CONST 1) (:CONST 0)) (make-array 1 :INITIAL-ELEMENT x))
-=> 0
-
-(EVALLI '(:IF (:CALL > ((:VAR 0) (:CONST 3))) (:CONST 1) (:CONST 0)) (make-array 1 :INITIAL-ELEMENT x))
-=> 1
-
-(defun test_plus (x) (+ x 3))
-(MEVAL '(test_plus 7))
-=> 10
-
-(defun fibonacci (n) (if (<= n 2) 1 (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
-(MEVAL '(fibonacci 6))
-=> 8
-
-(defun soustractionRecursive (x) (if (= x 0) x (- 1 (soustractionRecursive (- x 1)))))
-(MEVAL '(soustractionRecursive 3))
-=> 1
-
-(defun factorielle (n) (if (<= n 0) 1 (* n (factorielle (- n 1)))))
-(MEVAL '(factorielle 6))
-=> 720
-
-(defun test_if (x) (if (< x 3) 1 0))
-(MEVAL '(test_if 9))
-=> 0
-(MEVAL '(test_if 1))
-=> 1
-
-(MEVAL '(MEVAL '(fibonacci 6)))
-=> 8
-
-(MEVAL '(MEVAL '(MEVAL '(fibonacci 6))))
-=> 8
-
-(defun test_loop () (loop for i from 1 to 10 do (print i)))
-(MEVAL '(test_loop ))
-=> 1 2 3 4 5 6 7 8 9 10 NIL
-
-
-
-(defun EVALLI (expr env) 
-	(cond 
-		;Si c'est une constante
-		((eq (car expr ) :const ) ;On affiche le cdr (la valeur de la constante)
-			(car (cdr expr ) ) 
-		)
-		 ;Si c'est une variable
-		((eq (car expr ) :var ) ;On la récupère (sa valeur) dans le tableau des variables d'environnement
-		 	(aref env (car (cdr expr) ) ) 
-		 )
-		;Si c'est un setf
-		((eq (car expr ) :set-var )
-		;On récupère la variable dans le tableau des variables d'environnement
-		 	(aref env (second (cadr expr ) ) ) 
-		 ;Et on affecte à cette variable l'évaluation du cddr de l'expression en passant l'environnement courant 
-		  	(setf (EVALLI (cddr expr ) env ) ) 
-		)
-		;Si c'est une expression conditionnelle
-		((eq (car expr ) :if ) 
-			(if
-			;On évalue le test
-				(EVALLI (second expr ) env )
-				;On évalue l'expression de retour dans le cas positif du test
-				(EVALLI (third expr ) env ) 
-				;On évalue l'expression de retour dans le cas négatif du test
-				(EVALLI (fourth expr ) env ) 
+(defun make-vm (vm taille-pile)
+	(let(
+			(init-pile-donnees
+				(progn
+					(setf (get vm ':size) taille-pile)
+					(make-array (list taille-pile))
+				)
+			)
+			(init-pile-controle
+				(progn
+					(setf (get vm ':size) taille-pile)
+					(make-array (list taille-pile))
+				)
 			)
 		)
-		;Si c'est un appel de fonction primitive
-		(
-			(eq (car expr ) :call )
-			 ;On applique cette fonction primitive à la suite de l'expression
-			(apply (cadr expr ) (MAPEVALLI (first (cddr expr ) ) env ) ) 
-		)
-		;Si c'est une fonction inconnue on teste l'évaluation de la transcription en LI du reste de l'expression
-		((eq (car expr ) :unknown ) 
-			(EVALLI (LISPTOLI (second expr ) (third expr ) ) env ) 
-		)
-		;Si c'est un appel de fonction créée par l'utilisateur
-		((eq (car expr ) :mcall ) ;On affecte à une variable du nom de la fonction le retour de l'évaluation de l'expression
-			(let* 
-				((fun (GETLIDEFUN (second expr ) ) ) (nenv (make-array (+ 1 (car fun ) ) ) ) )
-				(EVALLI (car (cddr fun ) ) (MAKEENV (car (cddr expr ) ) env nenv 1 ) )
-			)
-		)
-		;Si c'est un progn d'une expression alors on évalue la suite de l'expression en ne retournant que la valeur du dernier appel récursif
-		((eq (car expr ) :progn)
-			(car (last (MAPEVALLI (car (cdr expr ) ) env ) ) )
-		)
+
+		(setf (get vm ':pile-controle) init-pile-controle)
+		(setf (get vm ':pile-donnees) init-pile-donnees)
+
+		;On stocke la taille de la pile de controle
+		(setf (aref (get vm ':pile-controle) 1) taille-pile))
+
+		;On stocke la taille de la pile de donnees
+		(setf (aref (get vm ':pile-controle) O) taille-pile))
+
+		;On stocke le sommet de la pile (SP Stack Pointer) SP >= base de pile car montante
+		(setf (aref (get vm ':pile-controle) 2) 0))
+
+		;On stocke le compteur de programme (PC Pointer Counter)
+		(setf (aref (get vm ':pile-controle) 3) -1))
+
+		;On stocke le pointeur de cadre (FP Frame Pointer) Définit les blocs de pile pour la structurer et faciliter les acces
+		(setf (aref (get vm ':pile-controle) 4) 0))
+
+		;On stocke la base de pile (BP Base Pointer) 
+		(setf (aref (get vm ':pile-controle) 5) 0))
+	)
+)
+
+(defun interpret-vm (vm expr)
+	(cond ((consp expr) 
+	    (if (equal (car expr) ':CONST) ;Empile le litteral
+	        ((setf (aref (get vm ':pile-donnees) (aref (get vm ':pile-controle) 2)) (cadr expr)) ;On empile le lit dans la pile de donnees en récupérant la position du SP
+	        (setf (aref (get vm ':pile-controle) 2) (+ (aref (get vm ':pile-controle) 2) 1))) ;On incrémente la position du SP
+	    )
+	  
+	    (if (equal (car expr) ':VAR) ;Empile le n ieme variable du bloc de pile courant
+	     	(setf (aref (get vm ':pile-donnees) (aref (get vm ':pile-controle) 2)) (aref (get vm ':pile-donnees) (cadr expr))) ;On empile la valeur de la n ieme variable
+	    	(setf (aref (get vm ':pile-controle) 2) (+ (aref (get vm ':pile-controle) 2) 1)) ;On incrémente la position du SP
+	    )
+	    
+	    (if (equal (car expr) ':SET-VAR) ;Affecte la valeur depilee a la n ieme variable du bloc de pile courant
+	     	(setf (aref (get vm ':pile-donnees) (cadr expr)) (aref (get vm ':pile-donnees) (aref (get vm ':pile-controle) 2))) ;On récupère la valeur du sommet de pile SP et on la met à la n ieme position dans la pile de donnees
+	    )
+	  
+	    (if (equal (car expr) ':STACK) ;Reserve un bloc de pile de taille n
+	     	(setf (aref (get vm ':pile-controle) 4) (+ (aref (get vm ':pile-controle) 4) (cadr expr))) ;On réserve un bloc de pile de taille n en faisant FP+n
+	     	)
+	  
+        (if (equal (car expr) ':CALL) ;Appelle la fonction f qui depile ses parametres et empile son resultat
+      		)
+  
+        (if (equal (car expr) ':RTN) ;Retourne d'un appel precedent
+     		)
+
+        (if (equal (car expr) ':SKIP) ;Saute n instructions
+     		(setf (aref (get nom-vm ':controlStack) 3) (+ (aref (get nom-vm ':controlStack) 3) (cadr expr)))) ;On incrémente le PC de n instructions
+  
+        (if (equal (car expr) ':SKIPNIL) ;Saute n instructions si la valeur depilee est NIL
+      		(if (equal (aref (get vm ':pile-donnees) (aref (get vm ':pile-controle) 2)) nil) ;Si le sommet de pile est NIL, on saute de n insctructions
+      			(setf (aref (get nom-vm ':pile-controle) 3) (+ (aref (get nom-vm ':pile-controle) 3) (cadr expr))))
+      	)
+	  
+	    (if (equal (car expr) ':SKIPTRUE) ;Saute n instructions si la valeur depilee n'est pas NIL
+      		(if (not (equal (aref (get vm ':pile-donnees) (aref (get vm ':pile-controle) 2)) nil)) ;Si le sommet de pile n'est pas NIL, on saute de n insctructions
+      			(setf (aref (get nom-vm ':pile-controle) 3) (+ (aref (get nom-vm ':pile-controle) 3) (cadr expr))))
+      	)	
+
+	    (if (equal (car expr) ':LOAD) ;Empile le contenu du mot mémoire d'adresse n
+	     	) ; ??   
+	  
+	    (if (equal (car expr) ':STORE) ;Affecte au mot memoire d'adresse n la valeur depilee
+	     	) ; ??
     )
 )
-
-(defun MAPEVALLI (lexpr env)
-	(if (null lexpr ) ;; si lexpr est nul
-		NIL ;; on retourne NIL
-		(cons (EVALLI (first lexpr ) env ) (MAPEVALLI (rest lexpr ) env ) ) ;; sinon on applique la fonction sur le reste des arguments
-	)
-)
-
-
-(defun MAKEENV (args env nenv index)
-	(if (null args )
-		nenv
-		(progn (setf (aref nenv index ) (EVALLI (car args ) env ) ) (MAKEENV (cdr args ) env nenv (+ 1 index ) ) )
-	)
-)
-
-(defun MEVAL (expr)
-	(EVALLI (LISPTOLI expr ()) () )
-)
-
-(defun GETLIDEFUN (fun) 
-	(get fun :defun )
-)
-
